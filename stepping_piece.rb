067c1256b48e1dbfa2a3323c783d7a02faefd0b5
Eric+Lily <eric_lily@appacademy.io>
2015-04-01 14:29:07 -0400

    finish valid_moves pawn

--- stepping_piece.rb
+++ stepping_piece.rb
@@ -48,17 +48,77 @@ class Pawn < SteppingPiece
   #   then check if diagonal capture possible
   #   and allow move two from starting row.
 
-  STEP_MOVES = [[0, 0]]
+  # STEP_MOVES = [[1, 0], [1, 1], [1, -1]]
+  #
+  # def moves
+  #   if color == :b
+  #     move_step(self.class::STEP_MOVES)
+  #   else
+  #     white_moves = STEP_MOVES
+  #     white_moves.map {:&first}
+  # end
+  #
+  # def move_step(directions)
+  #   moves = []
+  #
+  #   directions.each do |direction|
+  #     new_pos = position
+  #     if self.color == :b
+  #       new_pos = [new_pos[0] + direction[0], new_pos[1] + direction[1]]
+  #     else
+  #       new_pos = [new_pos[0] - direction[0], new_pos[1] + direction[1]]
+  #     end
+  #     next unless on_board?(new_pos)
+  #
+  #     if board[new_pos].nil?
+  #       moves << new_pos if (direction[1] == 0)
+  #     else
+  #       next if board[new_pos].color == color
+  #       moves << new_pos
+  #     end
+  #   end
+  #
+  #   moves
+  # end
 
-  def valid_moves
-    moves
+  def moves
+    i, row = self.color == :b ? [1, 1] : [-1, 6]
+
+    new_pos, new_moves = [], []
+    forward_pos = [position[0] + i, position[1]]
+    new_pos << forward_pos if on_board?(forward_pos)
+    new_moves += new_pos if board[new_pos[0]].nil?
+
+    new_moves << [position[0] + 2 * i, position[1]] if position[0] == row
+
+    capture_pos = []
+    left_pos = [position[0] + i, position[1] - 1]
+    right_pos = [position[0] + i, position[1] + 1]
+    capture_pos << left_pos if on_board?(left_pos)
+    capture_pos << right_pos if on_board?(right_pos)
+
+    capture_pos.each do |pos|
+      new_moves << pos unless board[pos].nil?
+    end
+
+    new_moves
+  end
     # selects possible_moves down to moves that
     # 1) don't result in check
+
     # 2) can only move diagonally to capture
     # 3) can only move forward twice from its starting row
+
     # 4) no piece blocking
     # 5) end_pos is empty
     # uses duped board to hypothetically execute move and evaluate
+
+  def valid_moves
+    [].tap do |valid_pos|
+      moves.select do |move|
+        valid_pos << move unless move_into_check?(move)
+      end
+    end
   end
 
 end
